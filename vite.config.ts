6c522f34a9e290a81ef60251d86905a89c6b1115
aldeobacchus <75472779+aldeobacchus@users.noreply.github.com>
2025-10-07 23:54:33 +0200

    ci: git change basepath for gh pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { imagetools } from 'vite-imagetools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/altn-portfolio/',
   plugins: [
     vue(),
     vueDevTools(),
